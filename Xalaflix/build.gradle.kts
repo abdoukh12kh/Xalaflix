e875d1527fc049e949cff12becbb9780ff1e2add
abdoukh12kh <zaaaa.b.d30@gmail.com>
2025-06-17 22:33:05 +0200

    Add files via upload

---
+++ Xalaflix/build.gradle.kts
@@ -0,0 +1,8 @@
+
+version = 1
+cloudstream {
+    language = "en"
+    description = "Xalaflix streaming extension with movies, series, subtitles, and multi-quality playback"
+    authors = listOf("YourName")
+    iconUrl = "https://xalaflix.io/favicon.ico"
+}
