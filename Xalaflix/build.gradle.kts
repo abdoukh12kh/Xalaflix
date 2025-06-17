0d5d52cb8b5b0a15028a9914d8a36360d7ae7a71
abdoukh12kh <zaaaa.b.d30@gmail.com>
2025-06-18 00:31:57 +0200

    Update build.gradle.kts

--- Xalaflix/build.gradle.kts
+++ Xalaflix/build.gradle.kts
@@ -3,9 +3,9 @@ plugins {
     id("kotlin-android")
     id("com.lagradost.cloudstream3.gradle")
 }
+
 cloudstream {
     language = "en"
-    version = 1          // ← Must be an integer!
-    description = "Xalaflix Plugin"
+    description = "Xalaflix plugin"
     authors = listOf("YourName")
 }
